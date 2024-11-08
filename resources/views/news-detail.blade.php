9083ecae970b63b26bb09f5e29ff10ce8f2dc982
aalfath7 <abdielhafizhy@gmail.com>
2024-11-08 22:14:21 +0700

    create model news

--- resources/views/news-detail.blade.php
+++ resources/views/news-detail.blade.php
@@ -2,8 +2,8 @@
     <x-header>Detail News</x-header>
 
     <div class="space-y-4 bg-white p-8 shadow rounded-lg">
-        <h1 class="font-bold text-2xl">{{ $item['title'] }}</h1>
-        <p>Author : {{ $item['author'] }}</p>
+        <h1 class="capitalize font-bold text-2xl">{{ $item['title'] }}</h1>
+        <p class="capitalize">{{ $item['author'] }} | {{ $item->created_at->diffForHumans() }}</p>
         <p>{{ $item['content'] }}</p>
     </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,8 @@
 <?php
 
+use App\Models\News;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
-
-class News{
-    public static function all(){
-        return [
-            [
-                'id' => '1',
-                'slug' => 'hari-ini-saya-membuat-database',
-                'title' => 'Hari ini saya membuat database',
-                'author' => 'farhan',
-                'content' => 'saya membuat aplikasi menggunakan laravel dengan database sqlite dan mulai belajar cara menggunakannya.'
-            ],
-            [
-                'id' => '2',
-                'slug' => 'hari-ini-saya-membuat-tampilan-aplikasi',
-                'title' => 'Hari ini saya membuat tampilan aplikasi',
-                'author' => 'farhan',
-                'content' => 'saya membuat tampilan aplikasi menggunakan framework laravel, salah satu framework bahasa pemrograman PHP.'
-            ],
-        ];
-    }
-}
 
 Route::get('/', function () {
     return view('home');
@@ -32,12 +12,8 @@ Route::get('/news', function () {
     return view('news', ['news' => News::all()]);
 });
 
-Route::get('/news/{slug}', function ($slug){
-    $item = Arr::first(News::all(), function($item) use ($slug){
-        return $item['slug'] == $slug;
-    });
-
-    return view('news-detail', ['item' => $item]);
+Route::get('/news/{news:slug}', function (News $news){
+    return view('news-detail', ['item' => $news]);
 });
 
 
